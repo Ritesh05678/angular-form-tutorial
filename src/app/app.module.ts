16f2bfde90f25dbd2a80d9fea8dbc699fd37bacb
Ritesh Satthya <129091350+Ritesh05678@users.noreply.github.com>
2023-04-05 10:37:42 -0700

    changes in the index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RadioButtonComponent } from './radio-button/radio-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RadioButtonComponent
   ],
   imports: [
     BrowserModule,
