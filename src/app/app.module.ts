2fc6ebb17cb9bf0f0ebc2845874f5271f692141f
Ritesh Satthya <129091350+Ritesh05678@users.noreply.github.com>
2023-04-05 10:47:30 -0700

    change rewr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RadioButtonComponent } from './radio-button/radio-button.component';
+import { SampleReactiveFormComponent } from './sample-reactive-form/sample-reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RadioButtonComponent
+    RadioButtonComponent,
+    SampleReactiveFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sample-reactive-form/sample-reactive-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-reactive-form',
+  templateUrl: './sample-reactive-form.component.html',
+  styleUrls: ['./sample-reactive-form.component.css']
+})
+export class SampleReactiveFormComponent {
+
+}
